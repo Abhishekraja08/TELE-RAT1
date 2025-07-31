30a48e85da8d1afee2415f92739498c9dc8f632e
Abhishekraja08 <abhishekraja0704@gmail.com>
2025-07-31 16:04:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const parsedId = Number(process.env.ID);
 module.exports = {
-  token: process.env.TOKEN || '8345746043:AAFkO4Aw9Aii4cO-RfQ-OMf_AXvXA4vZdJY',
+  token: process.env.TOKEN || '8171955798:AAHIEZDEgXLc7996Knj2oMMpt7GYe7E8_YI',
   id: isNaN(parsedId) ? 2037939353 : parsedId // replace 12345.. with your telegram chat id
 };
